95a35ffc197bffbd372695eae72e3f3d39fa718e
sangelito <129316135+sangelito@users.noreply.github.com>
2023-09-01 11:20:39 -0500

    forgot middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
