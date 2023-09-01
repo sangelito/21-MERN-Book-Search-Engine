2a9c20d7e745883698c47284d1dba94eb1f7fdbb
sangelito <129316135+sangelito@users.noreply.github.com>
2023-09-01 10:31:07 -0500

    import apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
