8f8f6cd8e2f6e02052eafa57ca6858cd18bdc894
sangelito <129316135+sangelito@users.noreply.github.com>
2023-09-01 16:18:10 -0500

    help restructuring sever js from BCS

--- server/server.js
+++ server/server.js
@@ -5,51 +5,41 @@ const { authMiddleware } = require("./utils/auth");
 const path = require('path');
 const db = require('./config/connection');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Create an Apollo Server instance
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-// server.applyMiddleware({ app });
+// Start the Apollo Server first
+async function startApolloServer() {
+  await server.start();
+  
+  // Apply Apollo Server middleware to Express
+  server.applyMiddleware({ app });
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+  // Serve the React client build in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    });
+  }
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build/index.html"));
-//   });
-// }
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
-// const startApolloServer = async (typeDefs, resolvers) => {
-//   await server.start();
-//   server.applyMiddleware({ app });
-
+  // Start the Express server
   db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     });
   });
+}
 
-
-
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
+startApolloServer();
